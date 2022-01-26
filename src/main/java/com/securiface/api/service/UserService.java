1c548ae72af512aeed7c6749dcf1954f4feeb9fe
dorinebertonTlse <dorineberton@gmail.com>
2022-01-26 21:18:47 +0100

    implement MVC users method

---
+++ src/main/java/com/securiface/api/service/UserService.java
@@ -0,0 +1,26 @@
+package com.securiface.api.service;
+
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.securiface.api.model.User;
+import com.securiface.api.repository.UserRepository;
+
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Objects.requireNonNull(username);
+        User user = userRepository.findUserWithName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user;
+    }
+}
