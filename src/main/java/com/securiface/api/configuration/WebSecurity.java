23ff23a78f7bf57a7afb5c3e4dd993d64ee585b4
dorinebertonTlse <dorineberton@gmail.com>
2022-01-31 10:26:18 +0100

    Minor fix

--- src/main/java/com/securiface/api/configuration/WebSecurity.java
+++ src/main/java/com/securiface/api/configuration/WebSecurity.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,10 +16,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
@@ -26,15 +25,12 @@ import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuc
 import com.securiface.api.service.AppAuthProvider;
 import com.securiface.api.service.UserService;
 
-@Configuration
 @EnableWebSecurity
+@Configuration
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
     @Autowired
     UserService userDetailsService;
-    
-    @Autowired
-    private AccessDeniedHandler accessDeniedHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception { 
@@ -65,7 +61,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		  auth.userDetailsService(userDetailsService);
+		  auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
 		  // test users
 		  /*
           auth.inMemoryAuthentication()
@@ -91,13 +87,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
             response.setStatus(HttpServletResponse.SC_OK);
         }
     }
-    /*
+
     // Password encode
     @Bean
     public PasswordEncoder passwordEncoder() {
        return new BCryptPasswordEncoder();
     }
-    */
+
     @Bean
     public AuthenticationProvider getProvider() {
         AppAuthProvider provider = new AppAuthProvider();
--- src/main/java/com/securiface/api/service/UserService.java
+++ src/main/java/com/securiface/api/service/UserService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.securiface.api.model.User;
 import com.securiface.api.repository.UserRepository;
 
+@Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     @Autowired
